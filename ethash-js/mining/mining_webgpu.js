379a0e824ea20a19e26ebf348a4b67da93a1a7e1
Tal Derei <Tad222@lehigh.edu>
2021-12-03 13:29:59 -0500

    trivial webGPU framework works

--- ethash-js/mining/mining_webgpu.js
+++ ethash-js/mining/mining_webgpu.js
@@ -1,5 +1,5 @@
 /** Ethash on WebGPU */
-const Ethash = require('../index.js')
+const Ethash = require('../index.js');
 
 /** 
  * Check if browser is WebGPU compatible 
@@ -11,57 +11,86 @@ if (!navigator.gpu) {
 /**
  * Perform GPU computations
  */
-async function computationGPU (inputs) {
+ (async () => {
     /** 
      * 1. Initialize WebGPU 
      */
     
-        /** navigator.gpu.requestAdapter() returns javascript promise, asynchronously resolving the GPU adapter */
+        /** navigator.gpu.requestAdapter() accesses the GPU: 
+         * returns javascript promise, asynchronously resolving with a GPU adapter 
+         */
         const adapter = await navigator.gpu.requestAdapter();
         if (!adapter) { 
+            console.log("Failed to get GPU adapter!");
             return; 
         }
-        /** adapter.requestDevice() returns a promise for a GPU device */
+        /** adapter.requestDevice() returns a promise that resolves with a GPU device */
         const device = await adapter.requestDevice();
 
     /** 
-     * 2. Allocate Buffer Memory Accessible by GPU (GPU Memory Space) 
+     * 2. Allocate Buffered Memory Accessible by GPU (GPU Memory Space) 
      */
 
         /** Size of memory (in bytes) */
-        const sizeOfMemory = 10; // change
+        const sizeOfMemory = 4; // change
 
         /** Data that need to perform calculations on */
         const data = 0; // change
 
-        /** device.createBuffer() creates a GPU buffer object mapped to a raw binary data buffer */
+        /** device.createBuffer() creates a GPU buffer object in a mapped state */
         const gpuBuffer = device.createBuffer({
+            mappedAtCreation: true,
             size: sizeOfMemory,
-            usage: GPUBufferUsage.STORAGE,
-            mappedAtCreation: true
+            usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.COPY_SRC,
         });
 
-        /** Raw data buffer can be retrieved by the GPU by calling the GPU buffer method getMappedRange() */
-        const floatBuffer = gpuBuffer.getMappedRange();
+        /** getMappedRange() is a GPU buffer method that retrieves the raw data buffer by the GPU */
+        const bufferStorage = gpuBuffer.getMappedRange();
 
         /** Write bytes to buffer */ 
-        new Float32Array(floatBuffer).set(data);
+        new Uint8Array(bufferStorage).set([0, 1, 2, 3]);
 
-        /** Unmap() prevents race conditions where GPU/CPU access memory at the same time -- GPU takes control */
+        /** Unmap() allows the GPU to take control -- and prevents race conditions where GPU/CPU access memory at the same time */
         gpuBuffer.unmap();
 
     /** 
-     * 3. Read Buffer Memory on GPU -- copy a GPU buffer to another GPU buffer and read it back
+     * 3. Read Buffer Memory from GPU -- copy a GPU buffer to another GPU buffer and read it back
      */
         
-        /** Second GPU buffer is created in an unmapped state this time with device.createBuffer() */
+        /** Second GPU buffer is created in an unmapped state this time */
         const gpuBufferNew = device.createBuffer({
             size: sizeOfMemory,
             usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.MAP_READ
         });
 
-    
     /**
-     * WORK IN PROGRESS
+     * 4. Create GPU Command Encoder -- encode commands for copying buffer to buffer
      */
-}
+
+        /** device.createCommandEncoder() returns javascript object that builds batch of buffered commands for the GPU */
+        const encoder = device.createCommandEncoder();
+
+        /** encoder.copyBufferToBuffer() adds the command to command queue for later execution */
+        encoder.copyBufferToBuffer(
+            gpuBuffer,     /** Source Buffer */
+            0,             /** Source Offset */
+            gpuBufferNew,  /** Destination Buffer */
+            0,             /** Destination Offset */
+            sizeOfMemory   /** Size of Memory */
+        );
+
+        /** Finish encoding and submit commands to GPU device command queue */
+        const commands = encoder.finish();
+        device.queue.submit([commands]);
+
+    /** 
+     * 5. Read Buffer From GPU
+     */
+
+        /** mapAsync() returns a promise that will resolve when the GPU buffer is mapped */
+        await gpuBufferNew.mapAsync(GPUMapMode.READ);
+
+        const copyBuffer = gpuBufferNew.getMappedRange();
+
+        console.log(new Uint8Array(copyBuffer));
+})();
